0dc13dcebb3b82292ba83374c8edf34b21c9fddc
tommiranta <tommi.ranta@gmail.com>
2024-10-20 13:26:06 +0300

    more fixes to setup

--- dot_oh-my-zsh/custom/custom.zsh
+++ dot_oh-my-zsh/custom/custom.zsh
@@ -3,30 +3,39 @@ SOLARIZED_THEME="dark"
 
 autoload -Uz compinit && compinit
 
-# Created by `pipx` on 2021-12-11 09:34:37
-export PATH="$PATH:/Users/tranta/.local/bin"
-autoload -U bashcompinit
-bashcompinit
-
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export PATH="/usr/local/opt/terraform@0.13/bin:$PATH"
-export PATH="/usr/local/opt/scala@2.12/bin:$PATH"
-export PATH="~/Library/Application Support/Coursier/bin:$PATH"
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="${PATH}:${HOME}/.krew/bin"
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Projects
-source /usr/local/bin/virtualenvwrapper.sh
+
+# pipx
+export PATH="$PATH:/Users/tranta/.local/bin"
+autoload -U bashcompinit
+bashcompinit
 
 # aliases
-alias gplm='git pull leadfeeder master'
 alias curl='noglob curl'
 
 # ruby
 source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
 source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
-chruby ruby-3.1.2
+
+# tenv
+export TENV_AUTO_INSTALL=true
+export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
+export TF_PLUGIN_CACHE_MAY_BREAK_DEPENDENCY_LOCK_FILE=true
+
+# 1Password completion
+eval "$(op completion zsh)"; compdef _op op
+
+# Fix resolving the main branch
+function git_main_branch() {
+  def=`git remote show origin | sed -n '/HEAD branch/s/.*: //p'`
+  echo $def
+}
+
--- setup_workstation.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-brew bundle
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
